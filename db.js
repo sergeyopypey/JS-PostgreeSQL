0fa6bc6bc6aa46e4a3ce64783b224f67881c3c2c
sergeyopypey <19.02.02@mail.ru>
2022-11-13 12:49:55 +0300

    2nd commit

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const pg = require('pg')
 const pool = new pg.Pool({
-    user: "postgres",
-    password: '3322',
-    host: "localhost",
-    port: 5433,
-    database: "node_postgres"
+    user: "postgres", // user which you autentificate in PSQL
+    password: '3322', // password from my database
+    host: "localhost", // type of the host where databases placed (?)
+    port: 5433, // port of your databases ( setted due installing )
+    database: "node_postgres" // name of your databases ( by default exists database with name = postgres )
 })
 
 module.exports = pool
